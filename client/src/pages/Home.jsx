22f3408baca7a5f3d10add061628c22643bdf389
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2022-12-05 13:47:42 +0200

    added more text to welcome/home/landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <div className="background">
       <h1>Good Morning Alef Lewitt!</h1>
-      <h2>What are you feeling this morning?</h2>
+      <h2>What are you FEELING this morning?</h2>
+      <h2>What THOUGHTS come to mind, related to this feeling?</h2>
+      <h2>What is your deep NEED from this place?</h2>
     </div>
   );
 }
