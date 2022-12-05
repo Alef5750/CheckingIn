48f63fa84bdea854fa591752bf2c4614617857b8
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2022-12-05 13:22:14 +0200

    added last name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Good Morning Alef!</h1>
+      <h1>Good Morning Alef Lewitt!</h1>
       <h2>What are you feeling this morning?</h2>
     </div>
   );
