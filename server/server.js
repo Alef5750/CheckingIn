b0bff9b8cb5bef82829a90a267dd39be7baca191
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2023-02-09 15:02:34 +0200

    controller function for adding a new entry

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const initDatabase = require("./database");
+const Entry = require("./entryModel");
+const entry = new Entry();
 
 // CORS Middleware
 const cors = require("cors");
@@ -13,15 +15,20 @@ app.use(
   })
 );
 
+// CONTROLLERS
+const addNewEntry = async (req, res) => {
+  const newEntry = req.body;
+  const entries = await entry.addNewEntry(newEntry);
+  if (entries) res.status(200).send(entries);
+  else res.status(400).send("Could not add to entries");
+};
+
 // BODY PARSER Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Create a new Entry
-app.post("/entries", (req, res) => {
-  const newEntry = req.body;
-  res.send("Got a POST request");
-});
+app.post("/entries", addNewEntry);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
